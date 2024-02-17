22824db43390ce8f312a43cbab949843a82ec5f1
Stefan Stanimirovic <stanimirovic.stefan@gmail.com>
2024-02-18 00:02:07 +0100

    Update barbershop.cpp

--- Source_01_Introduction/barbershop.cpp
+++ Source_01_Introduction/barbershop.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 using namespace std;
 
-// Ulaz: Dva vektora iste dužine: 
+// Ulaz: Dva vektora iste duzine: 
 // prices[i] predstavlja cenu goriva u i-tom gradu
 // distances[i] predstavlja cenu puta od grada i do grada (i + 1) % n
 // Izlaz: Vektor parova, gde prva koordinata predstavlja indeks grada
@@ -14,7 +14,7 @@ vector<pair<int, char>> barber_shop(const vector<double>& prices, const vector<d
 	vector<char> directions(n, '0'); // niz smerova
 	vector<double> newPrices(n, -1); // najjeftinije cene sipanja goriva
 
-	// Nađi minimalnu cenu goriva, i indeks grada gde je ta cena
+	// Nadji minimalnu cenu goriva, i indeks grada gde je ta cena
 	int minIndex = 0, minValue = prices[0];
 	for (int i = 1; i < n; i++) {
 		if (prices[i] < minValue) {
@@ -23,24 +23,24 @@ vector<pair<int, char>> barber_shop(const vector<double>& prices, const vector<d
 		}
 	}
 
-	// Podesi da meštani ovog grada sipaju gorivo u tom gradu
+	// Podesi da mestani ovog grada sipaju gorivo u tom gradu
 	indices[minIndex] = minIndex;
 	directions[minIndex] = '0';
 	newPrices[minIndex] = prices[minIndex];
 	
 	// Posmatramo smer suprotan od kretanja kazaljke na satu
 	// Za svaki grad koji prethodni minIndex gradu u ovom smeru
-	// (indeksi se povećavaju)
+	// (indeksi se povecavaju)
 	for (int i = (minIndex + 1) % n; i != minIndex; i = ((i + 1) % n)) {
-		// Ako se više isplati da odemo do susednog mesta nego da ostanemo u i-tom gradu 
+		// Ako se vise isplati da odemo do susednog mesta nego da ostanemo u i-tom gradu 
 		if (distances[(i + n - 1) % n] + newPrices[(i + n - 1) % n] < prices[i]) {
-			// Inače, plaćamo put do susednog grada i postupamo kao i ti meštani
+			// Placamo put do susednog grada i postupamo kao i ti mestani
 			newPrices[i] = distances[(i + n - 1) % n] + newPrices[(i + n - 1) % n];
 			indices[i] = indices[(i + n - 1) % n];
 			directions[i] = '-';
 		}
 		else {
-			// Ostajemo u i-tom gradu
+			// Inace, ostajemo u i-tom gradu
 			newPrices[i] = prices[i];
 			indices[i] = i;
 			directions[i] = '0';
@@ -51,9 +51,9 @@ vector<pair<int, char>> barber_shop(const vector<double>& prices, const vector<d
 	// Za svaki grad koji prethodni minIndex gradu u ovom smeru
 	// (indeksi se smanjuju)
 	for (int i = (minIndex + n - 1) % n; i != minIndex; i = ((i + n - 1) % n)) {
-		// Ako se više isplati da odemo do susednog mesta nego da ostanemo u i-tom gradu
+		// Ako se vise isplati da odemo do susednog mesta nego da ostanemo u i-tom gradu
 		if (distances[i] + newPrices[(i + 1) % n] < newPrices[i]) {
-			// Plaćamo put do susednog grada i postupamo kao i ti meštanu
+			// Placamo put do susednog grada i postupamo kao i ti mestani
 			newPrices[i] = distances[i] + newPrices[(i + 1) % n];
 			indices[i] = indices[(i + 1) % n];
 			directions[i] = '+';
