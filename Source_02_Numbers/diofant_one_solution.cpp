bcfcbda6c0c4f3b209ae1d0273ce81841554331e
Stefan Stanimirovic <stefanstanimirovic@users.noreply.github.com>
2024-03-06 17:40:42 +0100

    Create diofant_one_solution.cpp

---
+++ Source_02_Numbers/diofant_one_solution.cpp
@@ -0,0 +1,12 @@
+bool diofant_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
