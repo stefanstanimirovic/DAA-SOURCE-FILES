2f8fc9fc3f140fdab2b467a04687f0481a8cd7f6
Stefan Stanimirovic <stanimirovic.stefan@gmail.com>
2024-04-02 22:51:32 +0200

    Create find_peak.cpp

---
+++ Source_04_Divide_And_Conquer/find_peak.cpp
@@ -0,0 +1,25 @@
+int findPeak(int arr[], int n) {
+    if (arr[0] > arr[1]) {
+        return 0;
+    }
+    if (arr[n - 1] > arr[n - 2]) {
+        return n - 1;
+    }
+    int l = 1;
+    int r = n - 2;
+    int mid;
+
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (arr[mid - 1] < arr[mid] && arr[mid + 1] < arr[mid]) {
+            return mid;
+        }
+        if (arr[mid - 1] > arr[mid]) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
