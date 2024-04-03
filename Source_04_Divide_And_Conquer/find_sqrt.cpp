9bebf926345b78a921af1607fe61004de0b9676b
Stefan Stanimirovic <stanimirovic.stefan@gmail.com>
2024-04-03 13:33:31 +0200

    Create find_sqrt.cpp

---
+++ Source_04_Divide_And_Conquer/find_sqrt.cpp
@@ -0,0 +1,22 @@
+int findSqrt(int n) {
+    if (n < 2) {
+        return n;
+    }
+
+    int res, l = 1, r = n / 2;
+    while (l <= r) {
+        int m = (l + r) / 2;
+        long sqr = m * m;
+        if (sqr == n) {
+            return sqr;
+        }
+        else if (sqr < n) {
+            l = m + 1;
+            res = m;
+        }
+        else {
+            r = m - 1;
+        }
+    }
+    return res;
+}
