e7707bd6f0ecf17561c6426fb8b93938f9df72be
Stefan Stanimirovic <stanimirovic.stefan@gmail.com>
2024-04-09 14:14:28 +0200

    Create numberOfGrades2.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int numberOfGrades1(int a[], int n);
+int numberOfGrades2(int a[], int n);
 
 int main() {
     int a[] = { 5, 4, 4, 3, 3 };
     int n = sizeof(a) / sizeof(a[0]);
-    cout << numberOfGrades1(a, n);
+    cout << numberOfGrades1(a, n) << endl;
+    cout << numberOfGrades2(a, n) << endl;
     return 0;
 }
---
+++ Source_06_Greedy_Algorithms/numberOfGrades2.cpp
@@ -0,0 +1,22 @@
+int numberOfGrades2(int a[], int n) {
+	int ocene[6]; // koristimo elemente ocene[1], ..., ocene[5]
+	for (int i = 0; i <= 5; i++) {
+		ocene[i] = 0; // pocetne vrednosti za broj svake ocene
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i]; // zbir svih ocena
+		ocene[a[i]]++; // prebrojavanje svake ocene
+	}
+	int result = 0; // broj ocena koje treba popraviti
+	for (int k = 1; k <= 5; k++) {
+		for (int i = 0; i < ocene[k]; i++) {
+			if (sum > 4.5 * n) {
+				break;
+			}
+			sum += (5 - k); // popravlja ocenu k
+			result++; // broj popravljenih ocena se povecava za jedan
+		}
+	}
+	return result;
+}
