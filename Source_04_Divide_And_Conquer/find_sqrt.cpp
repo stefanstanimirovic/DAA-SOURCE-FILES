6911e765cf178bd6d8f8e0451555dc81de3d12b6
Stefan Stanimirovic <stanimirovic.stefan@gmail.com>
2024-04-21 12:06:46 +0200

    Update find_sqrt.cpp

--- Source_04_Divide_And_Conquer/find_sqrt.cpp
+++ Source_04_Divide_And_Conquer/find_sqrt.cpp
@@ -5,7 +5,7 @@ int findSqrt(int n) {
 
     int res, l = 1, r = n / 2;
     while (l <= r) {
-        int m = (l + r) / 2;
+        int m = l + (r - l) / 2;
         long sqr = m * m;
         if (sqr == n) {
             return sqr;
