28468d931fe207cd647317eb826a621b3c6393d3
Stefan Stanimirovic <stefanstanimirovic@users.noreply.github.com>
2025-07-19 16:40:40 +0200

    Update count_1s.cpp

--- Source_04_Divide_And_Conquer/count_1s.cpp
+++ Source_04_Divide_And_Conquer/count_1s.cpp
@@ -1,16 +1,20 @@
 int count1s(int a[], int n) {
-    // ako je poslednji element niza nula,
-    // tada niz ne sadrzi jedinice jer je sortiran
-    if (a[n - 1] == 0) {
-        return 0;
+    int l = 0;
+    int r = n - 1;
+    
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (a[mid] == 0) {
+            low = mid + 1;
+        } else {
+            if (mid == 0 || arr[mid - 1] == 0) {
+                return n - mid;
+            } else {
+                high = mid - 1;
+            }
+        }
     }
- 
-    // ako je prvi element niza jedinica,
-    // tada su svi elementi niza jedinice jer je sortiran
-    if (a[0]) {
-        return n;
-    }
- 
-    // podeli niz na levu i desnu particiju i rekurzivno pozovi za particije
-    return count(a, n/2) + count(a + n/2, n - n/2);
+
+    return 0; // nema jedinica u nizu
 }
